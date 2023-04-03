10f4818d267c824b6756b5970d36b920e015666d
idut2negra <49806480+idut2negra@users.noreply.github.com>
2023-04-04 01:23:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ tg.expand()
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = '${tg.initDataUnsafe.user.first_name}'
+
+usercard.appendChild(p)
+
+console.log('${tg.initDataUnsafe.user.first_name}')
+
 let item = "";
 
 let btn1 = document.getElementById("btn1");
@@ -23,11 +33,3 @@ btn1.addEventListener("click", function(){
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = '${tg.initDataUnsafe.user.first_name}'
-
-usercard.appendChild(p)
